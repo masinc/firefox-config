73cee4ba7a90e61fe24b2599526a028992c55b46
masinc <masinc000@gmail.com>
2023-03-24 02:40:01 +0900

    update

--- user.js
+++ user.js
@@ -1,6 +1,33 @@
-// user chrome
+// ssb
+user_pref("browser.ssb.enabled", true);
 
+// sessionstore
+user_pref("browser.sessionstore.interval", 15 * 60 * 1000);
+
+// fingerprint
+user_pref("privacy.resistFingerprinting", true);
+
+// userchrome
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// notify
+user_pref("dom.webnotifications.enabled", false);
+
+// platform
+user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0");
+user_pref("general.platform.override", "Win32");
+
+// buttery
+user_pref("dom.battery.enabled", false);
+
+// referer
+user_pref("network.http.sendRefererHeader", 1);
+user_pref("network.http.referer.XOriginPolicy", 1);
+user_pref("network.http.referer.spoofSource", true);
+user_pref("network.http.referer.trimmingPolicy", 2);
+
+// rendering delay
+user_pref("nglayout.initialpaint.delay", 0);
 
 // font
 user_pref("font.name.monospace.ja", "BIZ UDゴシック");
@@ -24,7 +51,6 @@ user_pref("gfx.font_rendering.directwrite.enabled", true);
 user_pref("gfx.use_text_smoothing_setting", true);
 
 // mouse
-
 user_pref("mousewheel.default.delta_multiplier_y", 400);
 
 // cache
