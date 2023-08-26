2471257c00f0e62797c5c97865defcacb2aee46b
masinc <masinc000@gmail.com>
2023-08-26 18:24:57 +0900

    refactor: format

--- user.js
+++ user.js
@@ -14,7 +14,10 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("dom.webnotifications.enabled", false);
 
 // platform
-user_pref("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0");
+user_pref(
+  "general.useragent.override",
+  "Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0"
+);
 user_pref("general.platform.override", "Win32");
 
 // buttery
@@ -39,7 +42,6 @@ user_pref("layers.acceleration.force-enabled", true);
 user_pref("gfx.direct2d.disabled", false);
 user_pref("gfx.direct2d.force-enabled", true);
 
-
 user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);
 user_pref("gfx.font_rendering.cleartype.always_use_for_content", true);
 user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);
@@ -54,6 +56,6 @@ user_pref("gfx.use_text_smoothing_setting", true);
 user_pref("mousewheel.default.delta_multiplier_y", 400);
 
 // cache
-user_pref('browser.cache.disk.enable', false);
-user_pref('browser.cache.memory.enable', true);
-user_pref('browser.cache.memory.capacity', -1);
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+user_pref("browser.cache.memory.capacity", -1);
